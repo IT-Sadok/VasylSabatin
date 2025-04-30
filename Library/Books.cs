2f38ea9ee4e95f19ace5f9bd3441ac226a1fb770
VasylSabatin <150041272+VasylSabatin@users.noreply.github.com>
2025-04-30 15:05:17 +0300

    Create .gitignore

---
+++ Library/Books.cs
@@ -0,0 +1,12 @@
+namespace Library;
+
+public class Books : Program
+{
+    public string Title { get; set; }
+    
+    public string Author { get; set; }
+    
+    public int Year { get; set; }
+    
+    public int Id { get; set; }
+}
---
+++ Library/Library.cs
@@ -0,0 +1,13 @@
+namespace Library;
+
+public class Program
+{
+    public var books = new Books();
+    public void AddBook()
+    {
+        int books.Title = "Shevchenko"
+    }
+    
+    Console.printline()
+
+}
