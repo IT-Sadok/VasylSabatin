5c0c9a69d7992b396725878b31fad482ab0a632d
VasylSabatin <vasiasabatin@gmail.com>
2025-07-28 14:41:37 +0300

    Create FieldLengths.cs

---
+++ MyWebApp/MyWebApp/Constants/FieldLengths.cs
@@ -0,0 +1,10 @@
+namespace MyWebApp.Constants;
+
+public class FieldLengths
+{
+    public const int DefaultText = 100;
+    
+    public const int ShortText = 50;
+    
+    public const int LongText = 1000;
+}
