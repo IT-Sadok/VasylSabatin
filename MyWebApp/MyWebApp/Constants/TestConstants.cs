d9fbc5085aff3e1ca8ec16dcd944c0b40cef0ad7
VasylSabatin <vasiasabatin@gmail.com>
2025-08-28 17:12:51 +0300

    Fix

--- MyWebApp/MyWebApp/Constants/TestConstants.cs
+++ MyWebApp/MyWebApp/Constants/TestConstants.cs
@@ -2,12 +2,6 @@ namespace MyWebApp.Constants;
 
 public static class TestConstants
 {
-    public const int WorkoutId1 = 1;
-    
-    public const int WorkoutId2 = 2;
-    
-    public const int NonExistingWorkoutId = 999;
-    
     public const int TestUserId = 1;
     
     public static readonly string TestUserIdString = TestUserId.ToString();
