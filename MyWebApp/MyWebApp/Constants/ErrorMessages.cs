a0abf13257154b03b99b2aa95ab0b31c20fed7b2
VasylSabatin <vasiasabatin@gmail.com>
2025-08-28 16:05:09 +0300

    WorkoutServiceTests

---
+++ MyWebApp/MyWebApp/Constants/ErrorMessages.cs
@@ -0,0 +1,10 @@
+namespace MyWebApp.Constants;
+
+public class ErrorMessages
+{ 
+        public const string DuplicateEmail = "Email is already taken. Please try another one.";
+        
+        public const string DuplicateUserName = "Username is already taken. Please try another one.";
+        
+        public const string DeleteDbError = "Could not delete user. Try again later.";
+}
